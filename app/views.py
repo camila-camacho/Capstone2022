dbe4542ceaf72917f54fd1091b2d860049a2bd5a
Camila <camachcf@dukes.jmu.edu>
2022-06-10 14:45:20 +0200

    Index almost done

--- app/views.py
+++ app/views.py
@@ -2,178 +2,178 @@ from app import app
 from flask import render_template
 
 
-@app.route("/")
+@app.route("/") 
 def index():
     return render_template("superio-html/index.html")
 
-@app.route("/about.html")
+@app.route("/about.html") 
 def about():
     return render_template("superio-html/about.html")
 
 
-@app.route("/blog-list-v1.html")
+@app.route("/blog-list-v1.html") 
 def blog_list_v1():
     return render_template("superio-html/blog-list-v1.html")
 
-@app.route("/3")
+@app.route("/blog-list-v2.html") 
 def blog_list_v2():
     return render_template("superio-html/blog-list-v2.html")
 
-@app.route("/4")
+@app.route("/blog-list-v3.html") 
 def blog_list_v3():
     return render_template("superio-html/blog-list-v3.html")
 
-@app.route("/5")
+@app.route("/blog_single.html") 
 def blog_single():
     return render_template("superio-html/blog_single.html")
 
 
-@app.route("/6")
+@app.route("/candidate-dashboard-applied-job.html") 
 def candidate_dashboard_applied_job():
     return render_template("superio-html/candidate-dashboard-applied-job.html")
 
-@app.route("/7")
+@app.route("/candidate-dashboard-cv-manager.html") 
 def candidate_dashboard_cv_manager():
     return render_template("superio-html/candidate-dashboard-cv-manager.html")
 
 
-@app.route("/8")
+@app.route("/candidate-dashboard-job-alerts.html") 
 def candidate_dashboard_job_alerts():
     return render_template("superio-html/candidate-dashboard-job-alerts.html")
 
-@app.route("/9")
+@app.route("/candidate-dashboard-profile.html") 
 def candidate_dashboard_profile():
     return render_template("superio-html/candidate-dashboard-profile.html")
 
-@app.route("/10")
+@app.route("/candidate-dashboard-resume.html") 
 def candidate_dashboard_resume():
     return render_template("superio-html/candidate-dashboard-resume.html")
 
-@app.route("/11")
+@app.route("/candidate-dashboard-shortlisted-resume.html") 
 def candidate_dashboard_shortlisted_resume():
     return render_template("superio-html/candidate-dashboard-shortlisted-resume.html")
 
-@app.route("/12")
+@app.route("/candidate-dashboard.html") 
 def candidate_dashboard():
     return render_template("superio-html/candidate-dashboard.html")
 
-@app.route("/17")
+@app.route("/candidates-list-v5.html") 
 def candidates_list_v5():
     return render_template("superio-html/candidates-list-v5.html")
 
-@app.route("/20")
+@app.route("/candidates-single-v3.html") 
 def candidates_single_v3():
     return render_template("superio-html/candidates-single-v3.html")
 
-@app.route("/21")
+@app.route("/contact.html") 
 def contact():
     return render_template("superio-html/contact.html")
 
-@app.route("/22")
+@app.route("/dashboard-applicants.html") 
 def dashboard_applicants():
     return render_template("superio-html/dashboard-applicants.html")
 
-@app.route("/23")
+@app.route("/dashboard-change-password.html") 
 def dashboard_change_password():
     return render_template("superio-html/dashboard-change-password.html")
 
-@app.route("/24")
+@app.route("/dashboard-company-profile.html") 
 def dashboard_company_profile():
     return render_template("superio-html/dashboard-company-profile.html")
 
-@app.route("/25")
+@app.route("/dashboard-manage-job.html")
 def dashboard_manage_job():
     return render_template("superio-html/dashboard-manage-job.html")
 
-@app.route("/26")
+@app.route("/dashboard-messages.html") 
 def dashboard_messages():
     return render_template("superio-html/dashboard-messages.html")
 
-@app.route("/27")
+@app.route("/dashboard-packages.html")
 def dashboard_packages():
     return render_template("superio-html/dashboard-packages.html")
 
-@app.route("/28")
+@app.route("/dashboard-post-job.html")
 def dashboard_post_job():
     return render_template("superio-html/dashboard-post-job.html")
 
-@app.route("/29")
+@app.route("/dashboard-resume-alerts.html")
 def dashboard_resume_alets():
     return render_template("superio-html/dashboard-resume-alerts.html")
 
-@app.route("/30")
+@app.route("/dashboard-resumes.html")
 def dashboard_resumes():
     return render_template("superio-html/dashboard-resumes.html")
 
-@app.route("/31")
+@app.route("/dashboard.html")
 def dashboard():
     return render_template("superio-html/dashboard.html")
 
-@app.route("/32")
+@app.route("/elements.html")
 def elements():
     return render_template("superio-html/elements.html")
 
-@app.route("/35")
+@app.route("/employers-list-v3.html")
 def employers_list_v3():
     return render_template("superio-html/employers-list-v3.html")
 
-@app.route("/36")
+@app.route("/employers-list-v4.html")
 def employers_list_v4():
     return render_template("superio-html/employers-list-v4.html")
 
-@app.route("/37")
+@app.route("/employers-single-v1.html")
 def employers_single_v1():
     return render_template("superio-html/employers-single-v1.html")
 
 
-@app.route("/40")
+@app.route("/faqs.html")
 def faqs():
     return render_template("superio-html/faqs.html")
     
-@app.route("/41")
+@app.route("/register.html")
 def register():
     return render_template("superio-html/register.html")
 
-@app.route("/42")
+@app.route("/register-popup.html")
 def register_popup():
     return render_template("superio-html/register-popup.html")
 
-@app.route("/43")
+@app.route("/pricing.html")
 def pricing():
     return render_template("superio-html/pricing.html")
 
-@app.route("/44")
+@app.route("/login.html")
 def login():
-    return render_template("/login.html")
+    return render_template("superio-html/login.html")
 
-@app.route("/45")
+@app.route("/login-popup.html")
 def login_popup():
-    return render_template("/login-popup.html")
+    return render_template("superio-html/login-popup.html")
 
-@app.route("/46")
+@app.route("/job-single.html")
 def job_single():
-    return render_template("/job-single.html")
+    return render_template("superio-html/job-single.html")
 
-@app.route("/47")
+@app.route("/job-single-2.html")
 def job_single_2():
-    return render_template("/job-single-2.html")
+    return render_template("superio-html/job-single-2.html")
 
-@app.route("/48")
+@app.route("/index-17.html")
 def index_17():
-    return render_template("/index-17.html")
+    return render_template("superio-html/index-17.html")
 
-@app.route("/49")
+@app.route("/index-10.html")
 def index_10():
-    return render_template("/index-10.html")
+    return render_template("superio-html/index-10.html")
 
-@app.route("/50")
+@app.route("/index-8.html")
 def index_8():
-    return render_template("/index-8.html")
+    return render_template("superio-html/index-8.html")
 
-@app.route("/51")
+@app.route("/index-7.html") 
 def index_7():
-    return render_template("/index-7.html")
+    return render_template("superio-html/index-7.html")
 
-@app.route("/52")
+@app.route("/index-6.html")
 def index_6():
-    return render_template("/index-6.html")
+    return render_template("superio-html/index-6.html")
