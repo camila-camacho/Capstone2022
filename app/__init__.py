1febeb3d9c85df471b89539f5eaf35933587706e
Camila <camachcf@dukes.jmu.edu>
2022-06-15 21:32:00 +0200

    some changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
+from firebase_admin import firestore, credentials, initialize_app
+
+cred = credentials.Certificate("app/capstone-33322-firebase-adminsdk-jeqfp-d78be2eaa3.json")
+firebase=initialize_app(cred)
+db=firestore.client()
+
 
 app = Flask(__name__)
 
 from app import views
+
+
